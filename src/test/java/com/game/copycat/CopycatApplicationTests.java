a39639734c31206ddcf2672fbfcdaa12e53c8bd5
growth <growth_s@naver.com>
2023-11-08 21:00:37 +0900

    [FEAT] 기본 UI 구성

---
+++ src/test/java/com/game/copycat/CopycatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.copycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopycatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
